58c8012521bdd36ed2d11e7983f92ab8ae87781b
Nobisuke19 <subham019650@gmail.com>
2025-12-26 15:52:27 +0530

    Use JWT_SECRET from env

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const SECRET = "YOUR_SUPER_SECRET_KEY"; // In production, use process.env.JWT_SECRET
+const SECRET = process.env.JWT_SECRET || "YOUR_SUPER_SECRET_KEY";
 
 exports.sign = (payload) => {
     return jwt.sign(payload, SECRET, { expiresIn: "1h" })
